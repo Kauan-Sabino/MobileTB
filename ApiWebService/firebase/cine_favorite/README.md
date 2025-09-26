9c42c8b5d03e2dd27850b37f43e7a023d81e3d31
Kauan-Sabino <kauan.oliveirakso@gmail.com>
2025-09-26 11:29:12 -0300

    26/09_conclusaoFormativa

--- ApiWebService/firebase/cine_favorite/README.md
+++ ApiWebService/firebase/cine_favorite/README.md
@@ -1,3 +1,125 @@
-# cine_favorite
+# cine_favorite (Formativa)
 
-A new Flutter project.
+## Briefing
+Construir um Aplicativo do Zero - O CineFavorite que permitirá criar uma conte e buscar filmes em uma API,
+montar uma galeria pessoal de filmes favoritos, com poster(capa) e nota avaliativa do usuario para o filme.
+
+## Objetivos
+- criar uma Galeria Personalizada por Usuário de Filmes Favoritos
+- buscar Filmes em uma API e listar para selecionar filmes favoritos
+- Criação de Contas por usuário
+- listar filmes por Palavra-Chave
+
+## Levantamento de Requisitos
+- ### Funcionais
+- ### Não Funcionais
+
+## Recurso do Projeto
+- Linguagem de Programação: Flutter/Dart
+- API TMDB: Base de dados pra filmes
+- Figma: prototipagem
+- GitHub: Para Armazenamento e versionamento do Código
+- FireBase: Authentication / FireStore DB
+- VsCode: codificação / Teste
+
+## Diagramas
+1. ### Classe:
+Demonstrar o funcionamento das Entidades do Sistema
+- Usuário (User:) classe já odelada pelo FireBaseAuth
+    - Atributs:email,senha,uid
+    - Métodos: login, registrar, logout
+
+- Filmes favoritos (Movie): Classe Modelada pelo DEV - baseada na API TMDB
+    - Atributos: id, título,PosterPath, Nota
+    - Métodos: Adicionar, Remover,Listar, atualizarNota
+    (CRUD)
+
+```mermaid
+
+classDiagram
+
+    class User{
+        +String uid,
+        +String email,
+        +String passoword
+        +login()
+        +logout()
+        +register()
+    }
+
+    class Movie{
+        +int id,
+        +String title
+        +String posterPath
+        +double rating
+        +addFavorite()
+        +removeFavorite()
+        +updateMovieRating()
+        +getFavoriteMovies()
+    }
+
+    User "1" -- "1+" Movie : "select"
+
+```
+
+2. ### Uso
+Ação que os atores podem fazer
+- Usuário:
+    - Regitrar
+    - Login
+    - Logout
+    - Procurar Filmes
+    - Salvar Filmes aos Favoritos
+    - Dar Nota aos Filmes Favorito
+    - Remover Filmes dos Favoritos
+
+``` mermaid
+
+graph TD
+    subgraph "Ações"
+        ac1([Registrar])
+        ac2([Login])
+        ac3([Logout])
+        ac4([SearchMovies])
+        ac5([AddFavoriteMovie])
+        ac6([UpdateMovieRating])
+        ac7([RemoveFavoriteMovie])
+    end
+
+    user([Usuário])
+
+    user --> ac1
+    user --> ac2
+
+    ac1 --> ac2
+    ac2 --> ac3
+    ac2 --> ac4
+    ac2 --> ac5
+    ac2 --> ac6
+    ac2 --> ac7
+
+
+
+
+``` 
+
+3. ### Fluxo
+
+Determinar o Caminho Percorrido pelo Ator paara excutar uma ação 
+
+- Fluxo da Ação de Login
+
+```mermaid
+
+    A[Início] --> B{tela de login}
+    B --> C[Inserir Email e Senha]
+    C --> D{Validar as Credenciais}
+    D --> Sim --> G[Favorite View]
+    D --> Não -- B
+```
+
+## Prototipagem
+
+link dos Protótipos
+
+## 
---
+++ ApiWebService/firebase/cine_favorite/lib/views/favorite_view.dart
@@ -0,0 +1,120 @@
+import 'dart:io';
+import 'dart:math';
+import 'dart:ui';
+
+import 'package:cine_favorite/controllers/movie_firestore_controller.dart';
+import 'package:cine_favorite/models/movie.dart';
+import 'package:cine_favorite/views/searchMovie_view.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_rating_bar/flutter_rating_bar.dart';
+
+class FavoriteView extends StatefulWidget {
+  const FavoriteView({super.key});
+
+  @override
+  State<FavoriteView> createState() => _FavoriteViewState();
+}
+
+class _FavoriteViewState extends State<FavoriteView> {
+  // atributo
+  final _movieFireStoreController = MovieFirestoreController();
+  double? _ratingValue;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Filmes Favoritos"),
+        actions: [
+          IconButton(
+            onPressed: FirebaseAuth.instance.signOut, 
+            icon: Icon(Icons.logout))
+        ],
+      ),
+      //criar uma gridView com os filmes favoritos
+      body: StreamBuilder<List<Movie>>(
+        stream: _movieFireStoreController.getFavoriteMovies()      , 
+        builder: (context, snapshot){
+          //se deu erro
+          if(snapshot.hasError) {
+            return Center(child: Text("Erro ao Carregar a Lista de Favoritos"),);
+          }
+          // enquanto carrega a lista
+          if(!snapshot.hasData){
+            return Center(child: CircularProgressIndicator(),);
+          }
+          //quando a lista esta vazia
+          if(snapshot.data!.isEmpty){
+            return Center(child: Text("Nenhum Filme Adicionado Aos Favoritos"),);
+          }
+          //a construção da lista
+          final favoriteMovies = snapshot.data!;
+          return GridView.builder(
+            padding: EdgeInsets.all(8),
+            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 2,
+              crossAxisSpacing: 8,
+              mainAxisSpacing: 8,
+              childAspectRatio: 0.7,),
+            itemCount: favoriteMovies.length, 
+            itemBuilder: (context, index){
+              //criar um obj de Movie
+              final movie = favoriteMovies[index];
+              return Card(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    Expanded(
+                      child: GestureDetector(
+                        onLongPress: () async {
+                          // colocar um alert de confirmação
+                          _movieFireStoreController.removeFavoriteMovie(movie.id);
+                          ScaffoldMessenger.of(context,).showSnackBar(SnackBar(content: Text("excluído dos favoritos")));
+                        },
+                        child: Image.file(
+                          File(movie.posterPath),
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                    ),
+                    // titulo do filme
+                    Center(child: Text(movie.title),),
+                    // nota do filme
+                    Center(child: Text(movie.rating.toString()),),
+                    //rating do filme
+                    RatingBar(
+                    initialRating: 0,
+                    direction: Axis.horizontal,
+                    allowHalfRating: true,
+                    itemCount: 5,
+                    ratingWidget: RatingWidget(
+                        full: const Icon(Icons.star, color: Colors.orange),
+                        half: const Icon(
+                          Icons.star_half,
+                          color: Colors.orange,
+                        ),
+                        empty: const Icon(
+                          Icons.star_outline,
+                          color: Colors.orange,
+                        )),
+                    onRatingUpdate: (value) {
+                      setState(() {
+                        _ratingValue = value;
+                      });
+                  }),
+                  ],
+                ),
+              );
+
+            });
+
+        }),
+      floatingActionButton: FloatingActionButton(
+        onPressed: ()=> Navigator.push(context, 
+          MaterialPageRoute(builder: (context)=>SearchMovieView())),
+        child: Icon(Icons.search),) ,
+    );
+  }
+}
+
